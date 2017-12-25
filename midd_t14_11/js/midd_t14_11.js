59b8de056e42939ca6b4b5811b706bc9f0ecd76d
upupdayday <sunhanqing1987@163.com>
2017-12-25 11:40:30 +0800

    modify

--- midd_t14_11/js/midd_t14_11.js
+++ midd_t14_11/js/midd_t14_11.js
@@ -2,10 +2,8 @@ $('.tabs li').on('click', function() {
     var $this = $(this)
     var index = $this.index()
     var panelWidth = $('.panel').width()
-    console.log(panelWidth)
-    console.log(index)
+
     $this.addClass('active').siblings().removeClass('active')
-    $this.parents('.mod-tab').find('.panels_ct')
+    $this.parents('.mod_tab').find('.panels_ct')
         .animate({ left: -panelWidth * index })
-        //$('.panels_ct').animate({ left: -panelWidth * index })
 })
